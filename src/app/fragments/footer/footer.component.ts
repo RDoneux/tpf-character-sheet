d82dd8e39bed69b01737c627951c60c7667e472b
Rob Doneux <robertdoneux@gmail.com>
2025-06-25 19:35:59 +0100

    feat: add warning if not logged in

--- src/app/fragments/footer/footer.component.ts
+++ src/app/fragments/footer/footer.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core'
+import { Component, DestroyRef, signal, WritableSignal } from '@angular/core'
 import { version } from '../../../../package.json'
 import { environment } from '../../../environments/environment'
+import { ISettings } from '../../services/settings/interfaces/i-settings'
+import { Store } from '@ngrx/store'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
     selector: 'app-footer',
-    imports: [],
+    imports: [MatIconModule],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
     version: string = version + environment.versionSuffix
+    isLoggedIn: WritableSignal<boolean> = signal(false)
+
+    constructor(
+        private store: Store<{ settings: ISettings }>,
+        private destroyRef: DestroyRef
+    ) {}
+
+    ngOnInit() {
+        this.store
+            .select((state: { settings: ISettings }) => state.settings)
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe((settings: ISettings) => this.isLoggedIn.set(settings.user != null))
+    }
 }
